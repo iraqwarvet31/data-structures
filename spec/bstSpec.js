796218e164521b55a9de7f960b1e0d243cfced1f
Larry Olguin <iraqwarvet31@gmail.com>
2021-03-21 09:41:16 -0700

    implemented 3 tests for bst

---
+++ spec/bstSpec.js
@@ -0,0 +1,29 @@
+describe('binarySearchTree', function() {
+  var binarySearchTree;
+
+  beforeEach(function() {
+    binarySearchTree = new BinarySearchTree(5);
+  });
+
+  it('Should have methods "insert", "search", "contains", "remove"', function() {
+    expect(binarySearchTree.insert).to.be.a("function");
+    expect(binarySearchTree.search).to.be.a("function");
+    expect(binarySearchTree.contains).to.be.a("function");
+    expect(binarySearchTree.remove).to.be.a("function");
+  });
+
+  it('BinarySearchTree should have _data, _left, and _right properties', function() {
+    expect(binarySearchTree).to.have.property('_data');
+    expect(binarySearchTree).to.have.property('_left');
+    expect(binarySearchTree).to.have.property('_right');
+  })
+
+  it('Should insert node to correct location of binary search tree', function() {
+    binarySearchTree.insert(11);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(7);
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(96);
+    expect(binarySearchTree._root._right._left).to.equal(7);
+  });
+})
---
+++ src/bst.js
@@ -0,0 +1,5 @@
+var BinarySearchTree = function(data) {
+  this._data = data;
+  this._left = null;
+  this._right = null;
+}
