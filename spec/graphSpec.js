4da805e6a6c79ebd6d35b707d034250015cff3b4
Larry Olguin <iraqwarvet31@gmail.com>
2021-05-09 15:03:33 -0700

    add 7 new tests to graphSpec.js

--- spec/graphSpec.js
+++ spec/graphSpec.js
@@ -1,11 +1,80 @@
 describe('graph', function() {
   var graph;
+  var expect = chai.expect;
 
   beforeEach(function() {
     graph = new Graph();
-  })
+  });
 
-  it('should have methods named "insertNode"', function() {
-    expect(graph.addNode).to.be.a('function');
-  })
-})
+  it('should have methods named "addVertex"', function() {
+    expect(graph.addVertex).to.be.a('function');
+  });
+
+  it('should add vertex to graph', function() {
+    graph.addVertex(1);
+    expect(graph.vertex.has(1)).to.equal(true);
+  });
+
+  it('should add edge to adjacency list', function() {
+    graph.addVertex(0);
+    graph.addVertex(1);
+    graph.addVertex(2);
+    graph.addVertex(3);
+    graph.addVertex(4);
+    graph.addEdge(0, 1);
+    graph.addEdge(0, 4);
+    graph.addEdge(1, 4);
+    graph.addEdge(1, 3);
+    graph.addEdge(1, 2);
+    graph.addEdge(2, 3);
+    graph.addEdge(3, 4);
+    expect(graph.vertex.get(0).includes(1)).to.equal(true);
+    expect(graph.vertex.get(1).includes(0)).to.equal(true);
+    expect(graph.vertex.get(3).includes(4)).to.equal(true);
+  });
+
+  it('should return true if graph contains value', function() {
+    graph.addVertex(105);
+    expect(graph.contains(105)).to.equal(true);
+  });
+
+  it('should return false if graph does not contain given value', function() {
+    graph.addVertex(105);
+    expect(graph.contains(3)).to.equal(false);
+  });
+
+  it('should remove edges between 2 vertices', function() {
+    graph.addVertex(0);
+    graph.addVertex(1);
+    graph.addEdge(0, 1);
+    graph.removeEdge(0, 1);
+    expect(graph.vertex.get(0).length).to.equal(0);
+    expect(graph.vertex.get(1).length).to.equal(0);
+  });
+
+  it('should remove given vertex and all it\'s edges', function() {
+    graph.addVertex(0);
+    graph.addVertex(1);
+    graph.addVertex(2);
+    graph.addVertex(3);
+    graph.addVertex(4);
+    graph.addEdge(0, 1);
+    graph.addEdge(0, 4);
+    graph.addEdge(1, 4);
+    graph.addEdge(1, 3);
+    graph.addEdge(1, 2);
+    graph.addEdge(2, 3);
+    graph.addEdge(3, 4);
+    graph.removeVertex(0);
+    expect(graph.vertex.has(0)).to.equal(false);
+    expect(graph.vertex.get(1).includes(0)).to.equal(false);
+    expect(graph.vertex.get(4).includes(0)).to.equal(false);
+  });
+});
+
+/*
+queue = 3, 4, 5, 6, 7
+visited =  0, 1, 2, 3, 4, 5, 6, 7
+
+
+*/
--- src/graph.js
+++ src/graph.js
@@ -1,3 +1,69 @@
 var Graph = function() {
-  this.nodes = new Map();
+  this.vertex = new Map();
+};
+
+Graph.prototype.addVertex = function(vertex) {
+  const itExists = this.vertex.get(vertex);
+
+  if (itExists) return null;
+
+  this.vertex.set(vertex, []);
+};
+
+Graph.prototype.addEdge = function(source, destination) {
+  const sourceList = this.vertex.get(source);
+  const destinationList = this.vertex.get(destination);
+
+  if (!sourceList || !destinationList) return null;
+
+  sourceList.push(destination);
+  destinationList.push(source);
+};
+
+Graph.prototype.contains = function(data) {
+  const sourceList = this.vertex.get(data);
+
+  if (sourceList) return true;
+
+  return false;
+};
+
+Graph.prototype.removeEdge = function(source, destination) {
+  let sourceList = this.vertex.get(source);
+  let destinationList = this.vertex.get(destination);
+
+  sourceList = sourceList
+    .filter(edge => edge !== destination);
+
+  destinationList = destinationList
+    .filter(edge => edge !== source);
+
+  this.vertex.set(source, sourceList);
+  this.vertex.set(destination, destinationList);
+};
+
+Graph.prototype.removeVertex = function(vertex) {
+  let sourceList = this.vertex.get(vertex);
+
+  sourceList.forEach((val, i) => {
+    let targetList = this.vertex.get(val);
+
+    targetList = targetList
+      .filter(edge => edge !== vertex);
+
+    this.vertex.set(val, targetList);
+  })
+
+  this.vertex.delete(vertex);
+};
+
+/*
+nodes = {
+  0: [1, 4],
+  1: [0, 4, 3, 2],
+  2: [1, 3],
+  3: [1, 4, 2],
+  4: [3, 0, 1]
 }
+
+*/
